de19492f94ab14893d827b6dd3f98fd521ac68b0
ThaoVy Nguyen <142556566+tvnguyen117@users.noreply.github.com>
2024-04-22 17:28:36 +0000

    Mon, Apr 22, 2024, 10:28 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -1,2 +1,8 @@
 const screens = document.querySelectorAll('.screen')
+const choose_insect_btns = document.querySelectorAll('.choose-insect-btn')
+const game_container = document.getElementById('game-container')
+const start_btn = document.getElementById('start-btn')
+const timeEl = document.getElementById('time')
+const scoreEl = document.getElementById('score')
+const message = document.getElementById('message')
 
